11424befe9127918fa6fd72e5fee361e1863cd05
unknown <onlyomkhotcs@gmail.com>
2024-12-24 17:01:18 +0530

    Url written directly

--- src/Helpers/AxiosSetup.js
+++ src/Helpers/AxiosSetup.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 console.log("hi");
-console.log("backend url",import.meta.env.VITE_BACKENDURL);
 
 const axiosInstance = axios.create({
-    baseURL : import.meta.env.VITE_BACKENDURL,
+    baseURL : 'https://sign-in-up-backend.onrender.com',
     headers : {
         'Content-Type' : 'application/json'
     }
