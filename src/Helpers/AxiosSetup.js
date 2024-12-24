3958af1b5ca9cacc460ccd75f65c0d664b03aa50
unknown <onlyomkhotcs@gmail.com>
2024-12-24 16:26:22 +0530

    Backend url

--- src/Helpers/AxiosSetup.js
+++ src/Helpers/AxiosSetup.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-console.log(import.meta.env.VITE_BACKEND_URL);
+console.log("hi");
+console.log("backend url",import.meta.env.VITE_BACKEND_URL);
 
 const axiosInstance = axios.create({
     baseURL : import.meta.env.VITE_BACKEND_URL,
